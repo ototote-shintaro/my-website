804a69b4de4dad917fe621bf9445c34171a7a70c
shintaro-tanaka <info@ototote.net>
2023-10-26 12:53:36 +0000

    supabase導入

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		domains: ["noliqyucceyolxirrwij.supabase.co"]
+	},
+}
 
 module.exports = nextConfig
