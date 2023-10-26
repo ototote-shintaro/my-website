7465d5ef87b85dbddd6e49296f2fefb21b92de63
shintaro-tanaka <info@ototote.net>
2023-10-26 09:29:04 +0000

    レイアウト作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className='py-2 px-8 border-t'>
+			<small>@2023 shintaro tanaka</small>
+		</footer>
+	)
+}
+
+export default Footer
---
+++ src/app/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+	return (
+		<header className='my-6'>
+			<h1 className='text-center'>Shintaro Tanaka</h1>
+		</header>
+	)
+}
+
+export default Header
