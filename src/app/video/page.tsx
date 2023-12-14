c14d54fe40725adf76f874124b519faa2d4cbf84
shintaro-tanaka <info@ototote.net>
2023-12-14 08:04:30 +0000

    ビデオ追加

--- src/app/video/page.tsx
+++ src/app/video/page.tsx
@@ -7,6 +7,7 @@ const page = () => {
 			<title>Video</title>
 			<h1 className='text-title'>Video</h1>
 			<div className='md:grid md:grid-cols-2 md:px-[5%]'>
+				<YoutubeContent videoId='h8uDkVQRhGY' label='Shintaro Tanaka: yatsure buro / 田中慎太郎: 窶風炉' />
 				<YoutubeContent videoId='QLeJ5uNi-_c' label='Kanako Fukui + Shintaro Tanaka / Improvisation Nov. 14' />
 				<YoutubeContent videoId='iI8b0GckrcQ' label='Shintaro Tanaka Winter Light Ensemble / Live at echo and cloud studio' />
 				<YoutubeContent videoId='hdodgI_VKzk' label='Shintaro Tanaka Ototote Ensemble / Live at Waseda Scott Hall' />
